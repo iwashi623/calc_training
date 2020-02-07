ddc470441ee50e34977760809aebde156bbf6471
SardineTa23 <kensho.iwashita.gude@gmail.com>
2020-02-07 22:16:09 +0900

    Home画面のレイアウト２

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,16 +10,31 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Image.asset("assets/images/image_title.png"),
-              Text("問題数を選択し、「スタート」ボタンを押してください"),
-              //TODO
-              RaisedButton(
-                child: Text("スタート"),
-              )
-            ],
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Image.asset("assets/images/image_title.png"),
+                Text("問題数を選択し、「スタート」ボタンを押してください"),
+                //TODO
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.bottomCenter,
+//                  padding: EdgeInsets.only(bottom: 10),
+                    child: RaisedButton.icon(
+                      color: Colors.brown,
+                      onPressed: () => print("ボタンの押したで〜"),
+                      label: Text("スタート"),
+                      icon: Icon(Icons.skip_next),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0))
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
